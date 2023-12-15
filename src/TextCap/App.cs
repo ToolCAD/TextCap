9f253b261e01a8079ab3a07bf3bda8f3d070a4e0
Hakim Ghods <ghods.hakim@hotmail.com>
2023-12-15 18:19:54 +0330

    Add project files.

---
+++ src/TextCap/App.cs
@@ -0,0 +1,48 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace TextCap
+{
+    public class App : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+
+            var loadCommandType = typeof(UpperCaseCommand);
+
+            var textCapPullDownButtonData = new PulldownButtonData(loadCommandType.FullName, "Text Cap")
+
+            {
+                // Set the LargeImage of levelsButton using an embedded resource
+                Image = new BitmapImage(new Uri("pack://application:,,,/TextCap;component/Resources/textcap-16.png", UriKind.RelativeOrAbsolute)),
+                LargeImage = new BitmapImage(new Uri("pack://application:,,,/TextCap;component/Resources/textcap-32.png", UriKind.RelativeOrAbsolute))
+            };
+
+            var panel = application.CreateRibbonPanel("Text Cap");
+
+            var pulldownButton=(PulldownButton)panel.AddItem(textCapPullDownButtonData);
+
+            AddPushButton(pulldownButton, typeof(UpperCaseCommand), "Upper Case");
+
+            return Result.Succeeded;
+        }
+
+        private static void AddPushButton(PulldownButton pullDownButton, Type command, string buttonText)
+        {
+            var buttonData = new PushButtonData(command.FullName, buttonText, Assembly.GetAssembly(command).Location, command.FullName);
+            pullDownButton.AddPushButton(buttonData);
+        }
+    }
+}
---
+++ src/TextCap/UpperCaseCommand.cs
@@ -0,0 +1,77 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextCap
+{
+    [Transaction(TransactionMode.Manual)]
+    public class UpperCaseCommand : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // Access the active Revit document
+            var uiDoc = commandData.Application.ActiveUIDocument;
+            var doc = uiDoc.Document;
+
+            Element pickedElement = null;
+
+            try
+            {
+                // Prompt user to select an element
+                var pickedElementRef = uiDoc.Selection.PickObject(ObjectType.Element);
+                pickedElement = doc.GetElement(pickedElementRef);
+
+                // Check if the picked element is a TextNote
+                if (pickedElement is TextNote textNote)
+                {
+                    // Get text from the TextNote
+                    string text = textNote.Text;
+
+                    // Get text from the TextNote and convert to uppercase
+                    string originalText = textNote.Text;
+                    string upperCaseText = originalText.ToUpper();
+
+                    // Set the text of the TextNote to uppercase
+                    using (Transaction tx = new Transaction(doc, "Change TextNote to Uppercase"))
+                    {
+                        tx.Start();
+                        textNote.Text = upperCaseText;
+                        tx.Commit();
+                    }
+
+
+
+                    // Do something with the text (e.g., print it)
+                    Debug.WriteLine("Text from the selected TextNote: " + text);
+                }
+                else
+                {
+                    Debug.WriteLine("The selected element is not a TextNote.");
+                    return Result.Failed;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Debug.WriteLine(ex.Message);
+                return Result.Failed;
+            }
+
+
+            if (pickedElement == null)
+            {
+                Debug.WriteLine("No element selected");
+                return Result.Failed;
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
