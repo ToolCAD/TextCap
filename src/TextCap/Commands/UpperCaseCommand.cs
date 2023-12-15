642795e9045387e10b0ab0ba3efb5e5987f6ac39
Hakim Ghods <ghods.hakim@hotmail.com>
2023-12-15 19:41:14 +0330

    all

--- src/TextCap/Commands/UpperCaseCommand.cs
+++ src/TextCap/Commands/UpperCaseCommand.cs
@@ -20,54 +20,89 @@ namespace TextCap
             var uiDoc = commandData.Application.ActiveUIDocument;
             var doc = uiDoc.Document;
 
-            Element pickedElement = null;
-
             try
             {
-                // Prompt user to select an element
-                var pickedElementRef = uiDoc.Selection.PickObject(ObjectType.Element);
-                pickedElement = doc.GetElement(pickedElementRef);
+                var selectedElements = uiDoc.Selection.GetElementIds();
 
-                // Check if the picked element is a TextNote
-                if (pickedElement is TextNote textNote)
+                if (selectedElements.Count > 0)
                 {
-                    // Get text from the TextNote
-                    string text = textNote.Text;
-
-                    // Get text from the TextNote and convert to uppercase
-                    string originalText = textNote.Text;
-                    string upperCaseText = originalText.ToUpper();
-
-                    // Set the text of the TextNote to uppercase
-                    using (Transaction tx = new Transaction(doc, "Change TextNote to Uppercase"))
+                    foreach (var selectedElementId in selectedElements)
                     {
-                        tx.Start();
-                        textNote.Text = upperCaseText;
-                        tx.Commit();
+                        Element selectedElement = doc.GetElement(selectedElementId);
+
+                        if (selectedElement is TextNote textNote)
+                        {
+                            // Get text from the TextNote and convert to uppercase
+                            string originalText = textNote.Text;
+                            string upperCaseText = originalText.ToUpper();
+
+                            // Set the text of the TextNote to uppercase
+                            using (Transaction tx = new Transaction(doc, "Change TextNote to Uppercase"))
+                            {
+                                tx.Start();
+                                textNote.Text = upperCaseText;
+                                tx.Commit();
+                            }
+
+                            // Do something with the text (e.g., print it)
+                            Debug.WriteLine("Text from the selected TextNote: " + upperCaseText);
+                        }
+                        else
+                        {
+                            continue;
+                        }
                     }
-
-
-
-                    // Do something with the text (e.g., print it)
-                    Debug.WriteLine("Text from the selected TextNote: " + text);
                 }
                 else
                 {
-                    Debug.WriteLine("The selected element is not a TextNote.");
-                    return Result.Failed;
+                    Element pickedElement = null;
+
+                    try
+                    {
+                        // Prompt user to select an element
+                        var pickedElementRef = uiDoc.Selection.PickObject(ObjectType.Element);
+                        pickedElement = doc.GetElement(pickedElementRef);
+
+                        // Check if the picked element is a TextNote
+                        if (pickedElement is TextNote textNote)
+                        {
+                            // Get text from the TextNote
+                            string text = textNote.Text;
+
+                            // Get text from the TextNote and convert to uppercase
+                            string originalText = textNote.Text;
+                            string upperCaseText = originalText.ToUpper();
+
+                            // Set the text of the TextNote to uppercase
+                            using (Transaction tx = new Transaction(doc, "Change TextNote to Uppercase"))
+                            {
+                                tx.Start();
+                                textNote.Text = upperCaseText;
+                                tx.Commit();
+                            }
+
+
+
+                            // Do something with the text (e.g., print it)
+                            Debug.WriteLine("Text from the selected TextNote: " + text);
+                        }
+                        else
+                        {
+                            Debug.WriteLine("The selected element is not a TextNote.");
+                            return Result.Failed;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+
+                        Debug.WriteLine(ex.Message);
+                        return Result.Failed;
+                    }
                 }
             }
             catch (Exception ex)
             {
-
                 Debug.WriteLine(ex.Message);
-                return Result.Failed;
-            }
-
-
-            if (pickedElement == null)
-            {
-                Debug.WriteLine("No element selected");
                 return Result.Failed;
             }
 
